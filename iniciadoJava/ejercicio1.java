467a403766d22d87b323d509c9d190803d4ac99e
Elgonzu2 <gonzafernandez94@gmail.com>
2023-09-28 16:02:58 -0300

    creando carpeta

---
+++ iniciadoJava/ejercicio1.java
@@ -0,0 +1,11 @@
+public class ejercicio1 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int suma = num1 + num2;
+
+        System.out.println("Resultado de la suma: " + suma);
+    }
+    
+}
---
+++ iniciadoJava/ejercicio4.java
@@ -0,0 +1,10 @@
+public class ejercicio4 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        int suma = num1 + num2;
+
+        System.out.println((num1 % num2 == 0) ? "El numero es divisible" + " la suma es: " + suma : "El numero no es divisible" + " la suma es: " + suma);
+    }
+}
