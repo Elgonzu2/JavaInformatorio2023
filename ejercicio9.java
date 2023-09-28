a556f80b5350a4ca93d09a40fe013593fd5c7e16
Elgonzu2 <gonzafernandez94@gmail.com>
2023-09-28 14:09:41 -0300

    nada especial

--- ejercicio9.java
+++ ejercicio9.java
@@ -2,8 +2,8 @@ public class ejercicio9 {
     public static void main(String[] args) {
         int num1 = 3;
 
-        int potencia1 = 3*3;
-        int potencia2 = 3*3*3;
+        int potencia1 = num1*num1;
+        int potencia2 = num1*num1*num1;
 
         System.out.println("elevado a 2: " + potencia1);
         System.out.println("elevado a 3: " + potencia2);
