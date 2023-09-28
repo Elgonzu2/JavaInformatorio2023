9a16c06d73970222fd0233f5305190d3ed554ab3
Elgonzu2 <gonzafernandez94@gmail.com>
2023-09-28 16:06:06 -0300

    eliminando

--- ejercicio6.java
+++
@@ -1,8 +0,0 @@
-public class ejercicio6 {
-    public static void main(String[] args) {
-        String nombre = "Pucca";
-        String pelicula = "El club de la pelea";
-
-        System.out.println("Nombre de la proxima bebida: " + nombre + pelicula);
-    }
-}
--- ejercicio8.java
+++
@@ -1,7 +0,0 @@
-public class ejercicio8 {
-    public static void main(String[] args) {
-        String nombre = "Gonzalo Fernandez";
-        int edad = 25;
-        System.out.println("Mi nombre es " + nombre + "\nMi edad es " + edad);
-    }
-}
