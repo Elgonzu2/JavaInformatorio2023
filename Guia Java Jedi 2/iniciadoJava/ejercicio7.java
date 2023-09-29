8eb53900df5830ba99d4db40b0f0549d744bb682
Elgonzu2 <gonzafernandez94@gmail.com>
2023-09-29 19:31:09 -0300

    ok

--- Guia Java Jedi 2/iniciadoJava/ejercicio7.java
+++ Guia Java Jedi 2/iniciadoJava/ejercicio7.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class ejercicio7 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Programa de Calificaciones");
+
+        System.out.print("Ingrese el número de notas: ");
+        int numeroNotas = scanner.nextInt();
+
+        double totalCalificaciones = 0;
+
+        System.out.println("Ingrese las notas (una por una):");
+
+        for (int i = 1; i <= numeroNotas; i++) {
+            System.out.print("Nota " + i + ": ");
+            double nota = scanner.nextDouble();
+            totalCalificaciones += nota;
+        }
+
+        double promedio = totalCalificaciones / numeroNotas;
+
+        System.out.print("Notas ingresadas: ");
+        for (int i = 1; i <= numeroNotas; i++) {
+            System.out.print(i + ": ");
+            if (i < numeroNotas) {
+                System.out.print(" | ");
+            }
+        }
+        System.out.println();
+        System.out.println("Promedio de calificaciones: " + promedio);
+
+        if (promedio >= 7.0) {
+            System.out.println("Aprobado");
+        } else {
+            System.out.println("Reprobado");
+        }
     }
 }
